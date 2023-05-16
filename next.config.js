2054b0f12150d3ca01dd719415ac8388c101b977
rokibulislam <rrcucse.11@gmail.com>
2023-05-17 04:28:34 +0600

    update api url

--- next.config.js
+++ next.config.js
@@ -6,13 +6,6 @@ const nextConfig = {
     Seatio_Secret: process.env.Seatio_Secret,
     Seatio_Public: process.env.Seatio_Public
   },
-  source: "/api/:path*",
-  headers: [
-    { key: "Access-Control-Allow-Credentials", value: "true" },
-    { key: "Access-Control-Allow-Origin", value: "*" },
-    { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-  ],
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
